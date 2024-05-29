c735fb3161cd6b96e452378af50debc2fd0c474a
juanflorezro <142103189+juanflorezro@users.noreply.github.com>
2024-05-28 20:39:22 -0500

    Update Hotel.jsx

--- src/views/Hotel.jsx
+++ src/views/Hotel.jsx
@@ -39,7 +39,7 @@ export default function Hotel() {
     }, []);
 
     // async function getHotel() {
-    //     await axios.get(`https://bkhoteles.juanflow04flore.repl.co/publicaciones/${ id }`)
+    //     await axios.get(`https://b23b48dc-fd4c-4332-8e69-5acdc3fda9f1-00-2abgkar42u9y2.sisko.replit.dev/publicaciones/${ id }`)
     //     .then(function (response) {
     //         // manejar respuesta exitosa
     //         response.data.comentarios = response.data.comentarios.reverse();
